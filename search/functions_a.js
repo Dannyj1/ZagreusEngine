782c10041b004dff24dbaca6ac2d2e0055a3742f
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2025-02-09 13:26:09 +0000

    Deploying to gh-pages from @ Dannyj1/Zagreus@5df11f9a5202134851d4abe6be24a7a2f1d79a3c 🚀

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,12 +1,11 @@
 var searchData=
 [
   ['main_0',['main',['../main_8cpp.html#a638b85ceffac097c2aa79b76ba8c055e',1,'main.cpp']]],
-  ['makemove_1',['makeMove',['../classZagreus_1_1Board.html#a7b701a74fc4664bf23d71ab49195b535',1,'Zagreus::Board']]],
-  ['makenullmove_2',['makeNullMove',['../classZagreus_1_1Board.html#ad4a10658afcd33c2504719f4466622fe',1,'Zagreus::Board']]],
-  ['mask_5fbishop_5fattacks_3',['mask_bishop_attacks',['../namespaceZagreus.html#a9822e1c29d5f304b6e3ff16d1c7a5fb9',1,'Zagreus']]],
-  ['mask_5frook_5fattacks_4',['mask_rook_attacks',['../namespaceZagreus.html#ab6c77e0b2ec848d790ff5aec8a292596',1,'Zagreus']]],
-  ['max_5',['max',['../classpcg__detail_1_1engine.html#afb796307012f539f9ac31eb4390a9ead',1,'pcg_detail::engine']]],
-  ['min_6',['min',['../classpcg__detail_1_1engine.html#a875ea02e42ccc2286b3966520e0faed5',1,'pcg_detail::engine']]],
-  ['movelist_7',['MoveList',['../structZagreus_1_1MoveList.html#a50020d74dad254fdb1aee79eb7c9c49b',1,'Zagreus::MoveList::MoveList()=default'],['../structZagreus_1_1MoveList.html#a4a6535c7c4c49b2dd88a584be042fd48',1,'Zagreus::MoveList::MoveList(const MoveList &amp;)=delete']]],
-  ['movepicker_8',['MovePicker',['../classZagreus_1_1MovePicker.html#a5f804b45810623a00beac6659f9b97a1',1,'Zagreus::MovePicker::MovePicker(MoveList &amp;moveList)'],['../classZagreus_1_1MovePicker.html#ab7ad6f3739b2754630a5c66dc866eb31',1,'Zagreus::MovePicker::MovePicker(const MovePicker &amp;)=delete']]]
+  ['makemove_1',['makeMove',['../classZagreus_1_1Board.html#abeabc3305abf62467e0678859dc85bf9',1,'Zagreus::Board']]],
+  ['mask_5fbishop_5fattacks_2',['mask_bishop_attacks',['../namespaceZagreus.html#a9822e1c29d5f304b6e3ff16d1c7a5fb9',1,'Zagreus']]],
+  ['mask_5frook_5fattacks_3',['mask_rook_attacks',['../namespaceZagreus.html#ab6c77e0b2ec848d790ff5aec8a292596',1,'Zagreus']]],
+  ['max_4',['max',['../classpcg__detail_1_1engine.html#afb796307012f539f9ac31eb4390a9ead',1,'pcg_detail::engine']]],
+  ['min_5',['min',['../classpcg__detail_1_1engine.html#a875ea02e42ccc2286b3966520e0faed5',1,'pcg_detail::engine']]],
+  ['movelist_6',['MoveList',['../structZagreus_1_1MoveList.html#a50020d74dad254fdb1aee79eb7c9c49b',1,'Zagreus::MoveList::MoveList()=default'],['../structZagreus_1_1MoveList.html#a4a6535c7c4c49b2dd88a584be042fd48',1,'Zagreus::MoveList::MoveList(const MoveList &amp;)=delete']]],
+  ['movepicker_7',['MovePicker',['../classZagreus_1_1MovePicker.html#a5f804b45810623a00beac6659f9b97a1',1,'Zagreus::MovePicker::MovePicker(MoveList &amp;moveList)'],['../classZagreus_1_1MovePicker.html#ab7ad6f3739b2754630a5c66dc866eb31',1,'Zagreus::MovePicker::MovePicker(const MovePicker &amp;)=delete']]]
 ];
