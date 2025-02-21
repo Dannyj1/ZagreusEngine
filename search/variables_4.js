b576ab959dab5a586c27e9b70a82c7c95ba245c8
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2025-02-21 09:04:54 +0000

    Deploying to gh-pages from @ Dannyj1/Zagreus@fe8a14ec9caf8a4f694155fb765cf188b2391cee 🚀

--- search/variables_4.js
+++ search/variables_4.js
@@ -8,6 +8,7 @@ var searchData=
   ['eg_5frook_5ftable_5',['eg_rook_table',['../namespaceZagreus.html#ab189515946f82df48dd4bd326e608c72',1,'Zagreus']]],
   ['endgamepsttable_6',['endgamePstTable',['../namespaceZagreus.html#a7b3cc214c8366a5b0a70e7743277d0a9',1,'Zagreus']]],
   ['enpassantsquare_7',['enPassantSquare',['../structZagreus_1_1BoardState.html#a8168f2482c44d4edf790701162e2a79f',1,'Zagreus::BoardState']]],
-  ['evalmaterialvalues_8',['evalMaterialValues',['../namespaceZagreus.html#a77212675e9ec1ea5afc27509b4afdd4a',1,'Zagreus']]],
-  ['evalmobility_9',['evalMobility',['../namespaceZagreus.html#a025a34fc4750a9af6e51d2c95b95c4d0',1,'Zagreus']]]
+  ['evalattacksaroundking_8',['evalAttacksAroundKing',['../namespaceZagreus.html#ab77fe0fb03ec0fc3624276aaf33fe0d6',1,'Zagreus']]],
+  ['evalmaterialvalues_9',['evalMaterialValues',['../namespaceZagreus.html#a77212675e9ec1ea5afc27509b4afdd4a',1,'Zagreus']]],
+  ['evalmobility_10',['evalMobility',['../namespaceZagreus.html#a025a34fc4750a9af6e51d2c95b95c4d0',1,'Zagreus']]]
 ];
--- search/variables_9.js
+++ search/variables_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['knightphase_0',['knightPhase',['../namespaceZagreus.html#ae2df05f6fadd8f4d561dc45821ddb549',1,'Zagreus']]]
+  ['kingarea_0',['kingArea',['../structZagreus_1_1EvalData.html#a5942408e5db6e6e5b3ac5789ec48adcd',1,'Zagreus::EvalData']]],
+  ['knightphase_1',['knightPhase',['../namespaceZagreus.html#ae2df05f6fadd8f4d561dc45821ddb549',1,'Zagreus']]]
 ];
